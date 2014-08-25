151a8029023032fb2dc07273235090748d23965e
Josh Pieper <jjp@pobox.com>
2014-08-25 06:15:32 -0400

    Start working towards a dynamics based test of the IMU constants.

--- imu/filter_constant_test.py
+++ imu/filter_constant_test.py
@@ -14,17 +14,68 @@ import imu_error_model
 
 SAMPLE_FREQUENCY_HZ = 100.0
 
+class TestCase(object):
+    def advance(self, dt):
+        raise NotImplementedError
+
+    def gyro(self):
+        raise NotImplementedError
+
+    def accel_yz(self):
+        raise NotImplementedError
+
+    def expected_pitch(self):
+        raise NotImplementedError
+
+class StationaryTest(TestCase):
+    def advance(self, dt):
+        pass
+
+    def gyro(self):
+        return 0.0
+
+    def accel_yz(self):
+        return 0., 1.
+
+    def expected_pitch(self):
+        return 0.
+
+class PendulumCase(TestCase):
+    def __init__(self):
+        self.length_m = 1.0
+        self.pitch_rad = 0.0
+        self.velocity_rad_s = 1.0
+
+    def gyro(self):
+        return self.velocity_rad_s
+
+    def accel_yz(self):
+        # The gravity component.
+        y = 1.0 * math.sin(self.pitch_rad)
+        z = 1.0 * math.cos(self.pitch_rad)
+
+        # The change in velocity component.
+
+
+    def advance(self, dt):
+        # TODO jpieper
+        raise NotImplementedError
+
+    def expected_pitch(self):
+        return self.pitch_rad
+
+
 def main():
     rng = random.Random()
 
-    # These constants are from the Pololu MiniIMU v2 @2g.
+    # These constants are from the Pololu MiniIMU v2 @8g.
     accel_y_error, accel_z_error = [
         imu_error_model.InertialErrorModel(
             rng,
             SAMPLE_FREQUENCY_HZ,
-            5.21e-3,
-            9.65e-5,
-            2.47e-5)
+            1.9e-2,
+            1.2e-3,
+            1.3e-5)
         for x in range(2)]
 
     # These constants are from the Pololu MiniIMU v2 at 2000dps
@@ -32,21 +83,21 @@ def main():
     gyro_error = imu_error_model.InertialErrorModel(
         rng,
         SAMPLE_FREQUENCY_HZ,
-        math.radians(0.0241),
-        math.radians(0.00808),
-        1.38e-5)
+        math.radians(0.001),  # 5.3e-4 rad/sqrt(s), 0.0303 deg/sqrt(s)
+        math.radians(0.001),  # 1.4e-4 rad/s, 0.0080 deg/s
+        1.38e-5)               # 3.6e-6 rad/sqrt(s)
 
-    estimator = ahrs.PitchEstimator(process_noise_gyro=math.radians(0.001)**2,
-                                    process_noise_bias=math.radians(0.01)**2,
-                                    measurement_noise_accel=0.1**2)
+    estimator = ahrs.PitchEstimator(process_noise_gyro=math.radians(0.05)**2,
+                                    process_noise_bias=math.radians(0.0512)**2,
+                                    measurement_noise_accel=0.05**2)
     estimator.process_accel(0, 1.0)
 
-    NUM_COUNT = 200000
+    NUM_COUNT = 100000
     error = 0.0
 
     for count in range(NUM_COUNT):
-        accel_y = accel_y_error.sample()
-        accel_z = accel_z_error.sample() + 1.0
+        accel_y = accel_y_error.sample() / 9.81
+        accel_z = (accel_z_error.sample() / 9.81) + 1.0
         gyro = gyro_error.sample()
 
         estimator.process_gyro(gyro)
