1ab431395ee324103a2561a0ced7d99cedca7891
theamk <fgitlab@theamk.com>
2014-08-30 17:02:19 -0400

    Add 'install-packages.sh' which installs required packages; Update deploy-vserver.sh to copy all the extra subdirectories.

--- video-ui/deploy-vserver.sh
+++ video-ui/deploy-vserver.sh
@@ -1,14 +1,50 @@
 #!/bin/bash
+# Last component must be named 'vserver'
+TMPDIR=/tmp/odroid-assemble/vserver
+
+if [[ "$1" != "start" && "$1" != "install" && "$1" != "assemble" && "$1" != "exec" ]]; then
+    cat <<EOF
+Usage: $0 command
+Commands:
+  start     -- upload server to odroid's RAM and start
+  install   -- install server to odroid's flash
+  assemble  -- do not upload, just put all files to $TMPDIR
+  exec CMD  -- upload, then exec CMD in vserver's dir
+EOF
+    exit 1
+fi
+
 set -e
-./vserver/vserver.py --check
-rsync --exclude '*~' -a --delete vserver odroid:/tmp/
-if [[ "$1" == "" ]]; then
-    echo Staring vserver
+cd $(dirname $(readlink -f "$0"))
+RELEASE=$(lsb_release -sc)
+if [[ "$RELEASE" != "precise" ]]; then
+    # The program cannot even be imported on precise
+    ./vserver/vserver.py --check
+fi
+
+mkdir -p $TMPDIR
+RSCMD=(rsync -a --exclude '*~' --exclude '*.pyc' --delete )
+
+
+"${RSCMD[@]}" --exclude=legtool --exclude=install-packages.sh \
+    --exclude=gbulb vserver $TMPDIR/..
+"${RSCMD[@]}" ../legtool ../install-packages.sh ../gbulb $TMPDIR
+
+if [[ "$1" == "assemble" ]]; then
+    echo Assembled
+    exit 0
+fi
+
+echo Uploading
+rsync -a --delete $TMPDIR odroid:/tmp/
+if [[ "$1" == "start" ]]; then
+    echo Starting vserver
     ssh odroid -t exec /tmp/vserver/start.sh
 elif [[ "$1" == "install" ]]; then
     echo Installing vserver to flash
     ssh odroid -t rsync -av --delete /tmp/vserver/ /home/odroid/vserver/
-else
+elif [[ "$1" = "exec" ]]; then
+    shift
     set -x
     ssh odroid -t cd /tmp/vserver \&\& "$@"
 fi
