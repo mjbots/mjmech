289d91ecf6a5085a1586183532735fd2cf0f263b
Mikhail A <fgitlab@theamk.com>
2014-09-12 00:56:11 -0400

    vclient: add UI state --- setting and logging; use it to draw reticle using SVG

--- video-ui/vclient.py
+++ video-ui/vclient.py
@@ -8,6 +8,8 @@ import optparse
 import json
 import errno
 import time
+import cStringIO as StringIO
+import functools
 
 import trollius as asyncio
 from trollius import Task, From
@@ -42,6 +44,9 @@ def wrap_event(callback):
             Gtk.main_quit()
             raise
     return wrapped
+
+def add_pair(a, b):
+    return (a[0] + b[0], a[1] + b[1])
 
 NULL_COMMAND = {
     'translate_x_mm_s': 0,
@@ -113,6 +118,7 @@ class UdpAnnounceReceiver(object):
 class ControlInterface(object):
     SEND_INTERVAL = 0.25
     VIDEO_PORT = 13357
+    UI_STATE_SAVEFILE = "last.jsonlist"
 
     def __init__(self, opts, host, port=13356):
         self.opts = opts
@@ -133,14 +139,58 @@ class ControlInterface(object):
             video_port=self.VIDEO_PORT,
             # Not set: turret - pair of (x, y) degrees
             # Not set: gait - a dict based off NULL_COMMAND
-            laser_on = 0,  # 0 = laser off; 1 = laser on
-            fire_cmd_count = 0,  # fire a shot every time this changes; should only ever increase
+
+            # 0 = laser off; 1 = laser on
+            laser_on = 0,
+
+            # fire a shot every time this changes; should only ever increase
+            fire_cmd_count = 0,
             mixer_on = 0,
             )
 
-        if opts.external_video:
-            self.video = None
-        else:
+        # state log file and last svg
+        self.state_log_file = None
+        self.last_svg_name = None
+        self.state_savefile_name = None
+        if opts.log_prefix:
+            # Last svg needs no timestamp
+            self.last_svg_name = os.path.join(
+                os.path.dirname(opts.log_prefix + 'test'), 'last.svg')
+            # Open file line-buffered
+            self.state_log_file = open(
+                opts.log_prefix + '.jsonlist', 'w', 1)
+            # Prepare savefile name
+            self.state_savefile_name = os.path.join(
+                os.path.dirname(opts.log_prefix + 'test'),
+                self.UI_STATE_SAVEFILE)
+
+        # Control UI state. Should be a simple dict, as we will be serializing
+        # it for later log replay.
+        self.state = dict(self._INITIAL_STATE)
+        self._logged_state = None
+
+        restore_from = opts.restore_state
+
+        # default -- only restore if file exists
+        if (restore_from is None
+            and self.state_savefile_name is not None
+            and os.path.exists(self.state_savefile_name)):
+            restore_from = self.state_savefile_name
+
+        if restore_from:    # not empty string or None
+            # Restore state
+            logging.info('Loading saved state from %r', restore_from)
+            with open(restore_from, 'r') as fh:
+                restored = json.load(fh)
+            self.state.update(restored)
+
+        self.video = None
+
+        # Log state before video, to check for code errors in rendering function
+        self._state_updated()
+
+
+        if not opts.external_video:
             video_log = None
             if opts.log_prefix:
                 video_log = opts.log_prefix + '.mkv'
@@ -148,6 +198,8 @@ class ControlInterface(object):
             self.video.on_video_click_1 = self._handle_video_click_1
             self.video.on_key_press = self._handle_key_press
             self.video.on_key_release = self._handle_key_release
+            self.video.on_got_video = functools.partial(
+                self._state_updated, force=True)
 
         def metric(device):
             abs_axis = device.get_features(linux_input.EV.ABS)
@@ -267,32 +319,51 @@ class ControlInterface(object):
             return
         ang_x, ang_y = old_turret
 
-        ang_x -= (pos[0] - 0.5) * self._CAMERA_VIEW_ANGLE_HOR
-        ang_y += (pos[1] - 0.5) * self._CAMERA_VIEW_ANGLE_VERT
+        ang_x -= (pos[0] - 0.5 - self.state['reticle_offset'][0]) \
+                 * self._CAMERA_VIEW_ANGLE_HOR
+        ang_y += (pos[1] - 0.5 - self.state['reticle_offset'][1]) \
+                 * self._CAMERA_VIEW_ANGLE_VERT
+        # TODO mafanasyev: add reticle_rotate support
 
         self.logger.info('Setting turret to (%+.1f, %+.1f) deg in response '
-                         'to click at %r',
-                         ang_x, ang_y, pos)
+                         'to click at (%.4f, %.4f)',
+                         ang_x, ang_y, pos[0], pos[1])
         self.control_dict['turret'] = (ang_x, ang_y)
         self._send_control()
 
+    _GAIT_KEYS = {  #  key->(dx,dy,dr)
+        'w': (0, -1, 0),
+        's': (0, 1, 0),
+        'a': (-1, 0, 0),
+        'd': (1, 0, 0),
+        'q': (0, 0, -1),
+        'e': (0, 0, 1)
+    }
+
+    _ARROW_KEYS = { # key->(dx, dy)
+        'Left': (-1, 0),
+        'Right': (1, 0),
+        'Up': (0, -1),
+        'Down': (0, 1)
+    }
+
     def _handle_key_press(self, evt):
-        dx = 0
-        dy = 0
-        dr = 0
-        name = Gdk.keyval_name(evt.keyval)
-        if name == 'w':
-            dy = 1
-        elif name == 's':
-            dy = -1
-        elif name == 'a':
-            dx = -1
-        elif name == 'd':
-            dx = 1
-        elif name == 'q':
-            dr = -1
-        elif name == 'e':
-            dr = 1
+        # Parse out the keys
+        base_name = Gdk.keyval_name(evt.keyval)
+        modifiers = ''
+        MT = Gdk.ModifierType
+        if evt.state & MT.CONTROL_MASK: modifiers += 'C-'
+        if evt.state & MT.SHIFT_MASK: modifiers += 'S-'
+        if evt.state & MT.MOD1_MASK: modifiers += 'M-'
+        name = modifiers + base_name
+
+        if name in self._GAIT_KEYS:
+            dx, dy, dr = self._GAIT_KEYS[name]
+            gait = NULL_COMMAND.copy()
+            gait['translate_x_mm_s'] = dx * 50
+            gait['translate_y_mm_s'] = dy * 100
+            gait['rotate_deg_s'] = dr * 30
+            self.control_dict['gait'] = gait
         elif name == 'h':
             self._print_help()
         elif name == 'l':
@@ -309,15 +380,23 @@ class ControlInterface(object):
         elif name == 'c':
             self.control_dict['turret'] = (0.0, 0.0)
             self.logger.info('Centered turret')
+        elif base_name in self._ARROW_KEYS and 'C-' in modifiers:
+            # Ctrl + arrows to move reticle center
+            dx, dy = self._ARROW_KEYS[base_name]
+            # Shift makes it slower
+            if 'S-' in modifiers:
+                step = 0.002
+            else:
+                step = 0.010
+            self.state['reticle_offset'] = add_pair(
+                self.state['reticle_offset'], (step * dx, step * dy))
+        elif name == 'r':
+            self.state['reticle_on'] ^= True
+            self.logger.info('Set reticle_on=%r', self.state['reticle_on'])
         else:
             self.logger.info('Unknown key %r' % name)
 
-        if dx or dy or dr:
-            gait = NULL_COMMAND.copy()
-            gait['translate_x_mm_s'] = dx * 50
-            gait['translate_y_mm_s'] = dy * 100
-            gait['rotate_deg_s'] = dr * 30
-            self.control_dict['gait'] = gait
+        self._state_updated()
         self._send_control()
 
     def _handle_key_release(self, evt):
@@ -334,6 +413,82 @@ class ControlInterface(object):
         print '  c        - enable && center turret'
         print '  click    - point turret to this location (must center first)'
         print '  ENTER    - fire'
+        print '  C+arrows - set reticle center (use shift for more precision)'
+        print '  r        - toggle reticle'
+
+    def _state_updated(self, force=False):
+        sertext = json.dumps(self.state, sort_keys=True)
+        if (sertext == self._logged_state) and not force:
+            return
+        self._logged_text = sertext
+
+        if self.state_log_file:
+            # TODO mafanasyev: add video timestamp
+            print >>self.state_log_file, sertext
+            # No need to flush
+
+        if self.state_savefile_name is not None:
+            with open(self.state_savefile_name + '~', 'w') as fh:
+                print >>fh, sertext
+            # Atomically install new version
+            os.rename(self.state_savefile_name + '~',
+                      self.state_savefile_name)
+
+
+        stream = StringIO.StringIO()
+        self._render_svg(stream, self.state)
+        if self.video:
+            self.video.set_svg_overlay(stream.getvalue())
+
+        if self.last_svg_name:
+            with open(self.last_svg_name + '~', 'w') as f:
+                f.write(stream.getvalue())
+            os.rename(self.last_svg_name + '~', self.last_svg_name)
+
+
+    _INITIAL_STATE = dict(
+        # SVG is stretched to video after rendering, so small width/height
+        # will make all lines thicker. Force to video resolution: 1920x1080
+        image_size=(1928, 1080),
+        reticle_on=True,
+        reticle_offset=(0, 0),
+        reticle_rotate=0,
+    )
+
+    @staticmethod
+    def _render_svg(out, state):
+        """Render SVG for a given state. Should not access anything other
+        than parameters, as this will be called during replay.
+        """
+        print >>out, '<svg width="{image_size[0]}" height="{image_size[1]}">'\
+            .format(**state)
+
+        if state['reticle_on']:
+            reticle_center_rel = add_pair((0.5, 0.5), state['reticle_offset'])
+            reticle_center = (
+                state['image_size'][0] * reticle_center_rel[0],
+                state['image_size'][1] * reticle_center_rel[1])
+
+            print >>out, '''
+<g transform='rotate({0[reticle_rotate]}) translate({1[0]} {1[1]})'
+   stroke="rgb(255,128,0)">
+  <line x1="500"  x2="100"  y1="0" y2="0" stroke-width="4" />
+  <line x1="-500" x2="-100" y1="0" y2="0" stroke-width="4" />
+  <line x1="-100" x2="100"  y1="0" y2="0" />
+  <line y1="500"  y2="100"  x1="0" x2="0" stroke-width="4" />
+  <line y1="-500" y2="-100" x1="0" x2="0" stroke-width="4" />
+  <line y1="-100" y2="100"  x1="0" x2="0" />
+
+  <line x1="-80" x2="80"  y1="-20" y2="-20" />
+  <line x1="-80" x2="80"  y1="20" y2="20" />
+  <line x1="-60" x2="60"  y1="40" y2="40" />
+  <line x1="-40" x2="40"  y1="60" y2="60" />
+  <line x1="-20" x2="20"  y1="80" y2="80" />
+</g>
+'''.format(state, reticle_center)
+
+        print >>out, '<text x="0" y="15" fill="red">Logs will go here</text>'
+        print >>out, '</svg>'
 
 
 class VideoWindow(object):
@@ -368,9 +523,9 @@ class VideoWindow(object):
         self.pipeline = Gst.Pipeline()
         self.rtpbin = self.make_element("rtpbin", do_retransmission=True)
 
-        #caps = Gst.Caps("application/x-rtp",
-        #                media="video", clock_rate=90000, encoding_name="H264")
-        caps = Gst.Caps.from_string("application/x-rtp,media=(string)video,clock-rate=(int)90000,encoding-name=(string)H264")
+        caps = Gst.Caps.from_string(
+            "application/x-rtp,media=(string)video,clock-rate=(int)90000,"
+            "encoding-name=(string)H264")
         rtp_src = self.make_element("udpsrc", caps=caps, port=self.port)
         self.link_pads(rtp_src, None, self.rtpbin, "recv_rtp_sink_0")
 
@@ -381,11 +536,14 @@ class VideoWindow(object):
                                       sync=False, async=False)
         self.link_pads(self.rtpbin, "send_rtcp_src_0", rtcp_sink, None)
 
-        self.textoverlay = self.make_element(
-            "textoverlay",
-            shaded_background=True, font_desc="16", auto_resize=False,
-            valignment="bottom", halignment="left", line_alignment="left",
-            text="Video info\nNo data yet")
+        #self.info_overlay = self.make_element(
+        #    "textoverlay",
+        #    shaded_background=True, font_desc="16", auto_resize=False,
+        #    valignment="bottom", halignment="left", line_alignment="left",
+        #    text="Video info\nNo data yet")
+        self.info_overlay = self.make_element(
+            "rsvgoverlay",
+            fit_to_frame=True)
 
         decode_elements = [
             self.make_element("rtph264depay"),
@@ -399,7 +557,8 @@ class VideoWindow(object):
             self.make_element("timeoverlay", shaded_background=True,
                               font_desc="8",
                               valignment="bottom", halignment="right"),
-            self.textoverlay,
+            self.info_overlay,
+            self.make_element("videoconvert"),
             ]
         if rotate:
             play_elements.append(self.make_element(
@@ -431,10 +590,11 @@ class VideoWindow(object):
         self.on_video_click_1 = None
         self.on_key_press = None
         self.on_key_release = None
+        self.on_got_video = None
 
     def make_element(self, etype, **kwargs):
         elt = Gst.ElementFactory.make(etype)
-        assert elt
+        assert elt, 'Failed to make element of type %r' % etype
         self.pipeline.add(elt)
         for n, v in sorted(kwargs.items()):
             elt.set_property(n.replace('_', '-'), v)
@@ -477,9 +637,14 @@ class VideoWindow(object):
 
     @wrap_event
     def _on_sync_message(self, bus, msg):
-        if msg.get_structure().get_name() == 'prepare-window-handle':
+        struct_name = msg.get_structure().get_name()
+        if struct_name == 'prepare-window-handle':
             self.logger.info('Embedding video window')
             msg.src.set_window_handle(self.xid)
+        else:
+            self.logger.debug('sync message: %r' % struct_name)
+            if self.on_got_video:
+                self.on_got_video()
 
     @wrap_event
     def _on_error(self, bus, msg):
@@ -492,7 +657,6 @@ class VideoWindow(object):
     @wrap_event
     def _on_end_of_stream(self, bus, msg):
         self.logger.info("End of stream: %r" % (msg, ))
-
 
     def _evt_get_video_coord(self, evt):
         # Get size of window
@@ -535,12 +699,14 @@ class VideoWindow(object):
         assert src == self.drawingarea, src
         rel_pos = self._evt_get_video_coord(evt)
         if rel_pos is None:
-            self.logger.info('Video click outside of image at wpt=(%d,%d) button=%d state=%d' %
-                             (evt.x, evt.y, evt.button, evt.state))
+            self.logger.info(
+                'Video click outside of image at wpt=(%d,%d) button=%d '
+                'state=%d', evt.x, evt.y, evt.button, evt.state)
             return True
 
-        self.logger.info('Video click at wpt=(%d,%d) rel=(%.3f,%.3f) button=%d state=%d' %
-                         (evt.x, evt.y, rel_pos[0], rel_pos[1], evt.button, evt.state))
+        self.logger.debug(
+            'Video click at wpt=(%d,%d) rel=(%.3f,%.3f) button=%d state=%d',
+            evt.x, evt.y, rel_pos[0], rel_pos[1], evt.button, evt.state)
         if evt.button == 1 and self.on_video_click_1:
             self.on_video_click_1(rel_pos, moved=False)
         return True
@@ -559,12 +725,17 @@ class VideoWindow(object):
             self.on_key_release(evt)
         return True
 
+    def set_svg_overlay(self, data):
+        self.info_overlay.set_property("data", data)
+
+
 def main(opts):
     asyncio.set_event_loop_policy(gbulb.GLibEventLoopPolicy())
 
     logging.basicConfig(
         level=logging.DEBUG,
-        format="%(asctime)s.%(msecs).3d [%(levelname).1s] %(name)s: %(message)s",
+        format=("%(asctime)s.%(msecs).3d "
+                "[%(levelname).1s] %(name)s: %(message)s"),
         datefmt="%T")
 
     root = logging.getLogger()
@@ -622,6 +793,9 @@ if __name__ == '__main__':
     parser.add_option('-L', '--log-dir', default='./mjmech-data',
                       help='Write logfiles using autogenerated names in '
                       'this directory. Empty to disable. Default "%default".')
+    parser.add_option('-R', '--restore-state', default=None,
+                      help='Restore UI state on startup. Default LOGDIR/' +
+                      ControlInterface.UI_STATE_SAVEFILE)
 
     opts, args = parser.parse_args()
     if len(args) and opts.addr is None:
