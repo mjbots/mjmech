a70b6cf8f68222185ce69114876b949b871e394a
Josh Pieper <jjp@pobox.com>
2014-09-14 18:05:52 -0400

    Add apache license text to all files in imu/ directory.

--- imu/test/test_quaternion.cc
+++ imu/test/test_quaternion.cc
@@ -1,4 +1,16 @@
 // Copyright 2014 Josh Pieper, jjp@pobox.com.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <boost/test/auto_unit_test.hpp>
 
@@ -62,7 +74,7 @@ BOOST_AUTO_TEST_CASE(QuaternionEulerAndBack) {
     double pitch_deg;
     double yaw_deg;
   };
-  
+
   TestCase tests[] = {
     {45, 0, 0},
     {0, 45, 0},
