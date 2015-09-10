1e2dbd45fc5d1ef59ab23cef8e2fc54e37d71c9f
Josh Pieper <jjp@pobox.com>
2015-09-10 06:54:40 -0400

    mech_warfare_command: A number of improvements:  * Provide a sign as part of the mapping.  * Actually pump the LinuxInput so that values change.  * Add a "verbose" mode, that writes to stdout the current command.

--- legtool/src/mech_warfare_command.cc
+++ legtool/src/mech_warfare_command.cc
@@ -32,9 +32,13 @@ typedef boost::asio::ip::udp udp;
 
 struct AxisMapping {
   int translate_x = -1;
+  int sign_translate_x = 1;
   int translate_y = -1;
+  int sign_translate_y = 1;
   int rotate = -1;
+  int sign_rotate = 1;
   int body_z = -1;
+  int sign_body_z = 1;
 };
 
 AxisMapping GetAxisMapping(const LinuxInput* input) {
@@ -46,27 +50,35 @@ AxisMapping GetAxisMapping(const LinuxInput* input) {
   AxisMapping result;
   if (features.capabilities.test(ABS_X)) {
     result.translate_x = ABS_X;
+    result.sign_translate_x = 1;
   }
   if (features.capabilities.test(ABS_Y)) {
     result.translate_y = ABS_Y;
+    result.sign_translate_y = -1;
   }
   if (features.capabilities.test(ABS_RX)) {
     result.rotate = ABS_RX;
+    result.sign_rotate = 1;
   }
   if (features.capabilities.test(ABS_RY)) {
     result.body_z = ABS_RY;
+    result.sign_body_z = -1;
   }
   return result;
 }
 
 struct Options {
   double period_s = 0.1;
-  double deadband = 0.1;
+  double deadband = 0.15;
   double max_translate_x_mm_s = 300.0;
   double max_translate_y_mm_s = 300.0;
   double max_rotate_deg_s = 100.0;
   double max_body_z_mm = 30.0;
   double min_body_z_mm = -30.0;
+  double idle_body_y_mm = 5.0;
+  double forward_body_y_mm = 15.0;
+  double reverse_body_y_mm = -5.0;
+  bool verbose = false;
 
   template <typename Archive>
   void Serialize(Archive* a) {
@@ -77,6 +89,9 @@ struct Options {
     a->Visit(LT_NVP(max_rotate_deg_s));
     a->Visit(LT_NVP(max_body_z_mm));
     a->Visit(LT_NVP(min_body_z_mm));
+    a->Visit(LT_NVP(forward_body_y_mm));
+    a->Visit(LT_NVP(reverse_body_y_mm));
+    a->Visit(LT_NVP(verbose));
   }
 };
 
@@ -106,22 +121,38 @@ class Commander {
   }
 
   void Start() {
+    StartRead();
+    StartTimer();
+  }
+
+  void StartRead() {
+    linux_input_->AsyncRead(
+        &event_, std::bind(&Commander::HandleRead, this, std::placeholders::_1));
+  }
+
+  void StartTimer() {
     timer_.expires_from_now(ConvertSecondsToDuration(options_.period_s));
     timer_.async_wait(std::bind(&Commander::HandleTimeout, this,
                                 std::placeholders::_1));
   }
 
  private:
+  void HandleRead(ErrorCode ec) {
+    FailIf(ec);
+    StartRead();
+  }
+
   void HandleTimeout(ErrorCode ec) {
     FailIf(ec);
-    Start();
+    StartTimer();
 
     Command command;
-    auto maybe_map = [this](double* destination, int mapping,
+    auto maybe_map = [this](double* destination, int mapping, int sign,
                             double minval, double maxval) {
       if (mapping < 0) { return; }
 
       double scaled = linux_input_->abs_info(mapping).scaled();
+      scaled *= sign;
       if (std::abs(scaled) < options_.deadband) {
         scaled = 0.0;
       } else if (scaled > 0.0) {
@@ -130,17 +161,22 @@ class Commander {
         scaled = (scaled + options_.deadband) / (1.0 - options_.deadband);
       }
 
-      double value = ((scaled < 0) ? minval : maxval) * scaled;
+      const double value = ((scaled < 0) ? minval : maxval) * std::abs(scaled);
+
       *destination = value;
     };
 
     maybe_map(&command.translate_x_mm_s, mapping_.translate_x,
+              mapping_.sign_translate_x,
               -options_.max_translate_x_mm_s, options_.max_translate_x_mm_s);
     maybe_map(&command.translate_y_mm_s, mapping_.translate_y,
+              mapping_.sign_translate_y,
               -options_.max_translate_y_mm_s, options_.max_translate_y_mm_s);
     maybe_map(&command.rotate_deg_s, mapping_.rotate,
+              mapping_.sign_rotate,
               -options_.max_rotate_deg_s, options_.max_rotate_deg_s);
     maybe_map(&command.body_z_mm, mapping_.body_z,
+              mapping_.sign_body_z,
               options_.min_body_z_mm, options_.max_body_z_mm);
 
     const bool active =
@@ -148,6 +184,25 @@ class Commander {
         command.translate_y_mm_s != 0.0 ||
         command.rotate_deg_s != 0;
     command.lift_height_percent = active ? 100.0 : 0.0;
+
+    if (active) {
+      if (command.translate_y_mm_s > 0.0) {
+        command.body_y_mm = options_.forward_body_y_mm;
+      } else if (command.translate_y_mm_s < 0.0) {
+        command.body_y_mm = options_.reverse_body_y_mm;
+      } else {
+        command.body_y_mm = options_.idle_body_y_mm;
+      }
+    }
+
+    if (options_.verbose) {
+      std::cout << boost::format("x=%4.0f y=%4.0f r=%4.0f z=%4.0f\r") %
+          command.translate_x_mm_s %
+          command.translate_y_mm_s %
+          command.rotate_deg_s %
+          command.body_z_mm;
+      std::cout.flush();
+    }
 
     std::string message = SerializeCommand(command);
     socket_->send_to(boost::asio::buffer(message), target_);
@@ -161,6 +216,7 @@ class Commander {
   boost::asio::deadline_timer timer_;
   const AxisMapping mapping_;
 
+  LinuxInput::Event event_;
 };
 
 int work(int argc, char** argv) {
