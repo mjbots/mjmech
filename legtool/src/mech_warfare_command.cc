6d42d68eec3aadd25dcc08b2661ff2010265e2da
Josh Pieper <jjp@pobox.com>
2015-09-09 22:12:44 -0400

    mech_warfare_command: Try to support joysticks, although untested so far.

--- legtool/src/mech_warfare_command.cc
+++ legtool/src/mech_warfare_command.cc
@@ -12,17 +12,156 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <linux/input.h>
+
 #include <boost/asio.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/program_options.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
+#include "fail.h"
 #include "gait.h"
+#include "linux_input.h"
 #include "program_options_archive.h"
 #include "property_tree_archive.h"
 
 namespace {
 using namespace legtool;
+namespace pt = boost::property_tree;
+typedef boost::asio::ip::udp udp;
+
+struct AxisMapping {
+  int translate_x = -1;
+  int translate_y = -1;
+  int rotate = -1;
+  int body_z = -1;
+};
+
+AxisMapping GetAxisMapping(const LinuxInput* input) {
+  auto features = input->features(EV_ABS);
+
+  // For now, we'll just hard-code the mapping used by jpieper's xbox
+  // controller.  In the long term, or even medium term, this should
+  // probably be configurable somehow.
+  AxisMapping result;
+  if (features.capabilities.test(ABS_X)) {
+    result.translate_x = ABS_X;
+  }
+  if (features.capabilities.test(ABS_Y)) {
+    result.translate_y = ABS_Y;
+  }
+  if (features.capabilities.test(ABS_RX)) {
+    result.rotate = ABS_RX;
+  }
+  if (features.capabilities.test(ABS_RY)) {
+    result.body_z = ABS_RY;
+  }
+  return result;
+}
+
+struct Options {
+  double period_s = 0.1;
+  double deadband = 0.1;
+  double max_translate_x_mm_s = 300.0;
+  double max_translate_y_mm_s = 300.0;
+  double max_rotate_deg_s = 100.0;
+  double max_body_z_mm = 30.0;
+  double min_body_z_mm = -30.0;
+
+  template <typename Archive>
+  void Serialize(Archive* a) {
+    a->Visit(LT_NVP(period_s));
+    a->Visit(LT_NVP(deadband));
+    a->Visit(LT_NVP(max_translate_x_mm_s));
+    a->Visit(LT_NVP(max_translate_y_mm_s));
+    a->Visit(LT_NVP(max_rotate_deg_s));
+    a->Visit(LT_NVP(max_body_z_mm));
+    a->Visit(LT_NVP(min_body_z_mm));
+  }
+};
+
+std::string SerializeCommand(const Command& command_in) {
+  Command command = command_in;
+  std::ostringstream ostr;
+  pt::ptree tree;
+  tree.add_child("gait", PropertyTreeWriteArchive().Accept(&command).tree());
+  pt::write_json(ostr, tree);
+  return ostr.str();
+}
+
+class Commander {
+ public:
+  Commander(const Options& options,
+            boost::asio::io_service& service,
+            const udp::endpoint& target,
+            LinuxInput* linux_input,
+            udp::socket* socket)
+      : options_(options),
+        service_(service),
+        target_(target),
+        linux_input_(linux_input),
+        socket_(socket),
+        timer_(service),
+        mapping_(GetAxisMapping(linux_input)) {
+  }
+
+  void Start() {
+    timer_.expires_from_now(ConvertSecondsToDuration(options_.period_s));
+    timer_.async_wait(std::bind(&Commander::HandleTimeout, this,
+                                std::placeholders::_1));
+  }
+
+ private:
+  void HandleTimeout(ErrorCode ec) {
+    FailIf(ec);
+    Start();
+
+    Command command;
+    auto maybe_map = [this](double* destination, int mapping,
+                            double minval, double maxval) {
+      if (mapping < 0) { return; }
+
+      double scaled = linux_input_->abs_info(mapping).scaled();
+      if (std::abs(scaled) < options_.deadband) {
+        scaled = 0.0;
+      } else if (scaled > 0.0) {
+        scaled = (scaled - options_.deadband) / (1.0 - options_.deadband);
+      } else {
+        scaled = (scaled + options_.deadband) / (1.0 - options_.deadband);
+      }
+
+      double value = ((scaled < 0) ? minval : maxval) * scaled;
+      *destination = value;
+    };
+
+    maybe_map(&command.translate_x_mm_s, mapping_.translate_x,
+              -options_.max_translate_x_mm_s, options_.max_translate_x_mm_s);
+    maybe_map(&command.translate_y_mm_s, mapping_.translate_y,
+              -options_.max_translate_y_mm_s, options_.max_translate_y_mm_s);
+    maybe_map(&command.rotate_deg_s, mapping_.rotate,
+              -options_.max_rotate_deg_s, options_.max_rotate_deg_s);
+    maybe_map(&command.body_z_mm, mapping_.body_z,
+              options_.min_body_z_mm, options_.max_body_z_mm);
+
+    const bool active =
+        command.translate_x_mm_s != 0.0 ||
+        command.translate_y_mm_s != 0.0 ||
+        command.rotate_deg_s != 0;
+    command.lift_height_percent = active ? 100.0 : 0.0;
+
+    std::string message = SerializeCommand(command);
+    socket_->send_to(boost::asio::buffer(message), target_);
+  }
+
+  const Options options_;
+  boost::asio::io_service& service_;
+  const udp::endpoint target_;
+  LinuxInput* const linux_input_;
+  udp::socket* const socket_;
+  boost::asio::deadline_timer timer_;
+  const AxisMapping mapping_;
+
+};
 
 int work(int argc, char** argv) {
   boost::asio::io_service service;
@@ -30,15 +169,20 @@ int work(int argc, char** argv) {
   namespace po = boost::program_options;
 
   std::string target = "192.168.0.123";
+  std::string joystick;
   Command command;
+  Options options;
 
   po::options_description desc("Allowable options");
   desc.add_options()
       ("help,h", "display usage message")
       ("target,t", po::value(&target), "destination of commands")
+      ("joystick,j", po::value(&joystick),
+       "send live commands from joystick at this device")
       ;
 
   ProgramOptionsArchive(&desc, "cmd.").Accept(&command);
+  ProgramOptionsArchive(&desc, "opt.").Accept(&options);
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -60,21 +204,23 @@ int work(int argc, char** argv) {
     port_str = "13356";
   }
 
-  namespace pt = boost::property_tree;
-
-  std::ostringstream ostr;
-  pt::ptree tree;
-  tree.add_child("gait", PropertyTreeWriteArchive().Accept(&command).tree());
-  pt::write_json(ostr, tree);
-  std::string message = ostr.str();
-
-  typedef boost::asio::ip::udp udp;
-
   udp::resolver resolver(service);
   auto it = resolver.resolve(udp::resolver::query(host, port_str));
 
   udp::socket socket(service, udp::v4());
-  socket.send_to(boost::asio::buffer(message), *it);
+
+  if (joystick.empty()) {
+    auto message = SerializeCommand(command);
+    socket.send_to(boost::asio::buffer(message), *it);
+  } else {
+    LinuxInput input(service, joystick);
+
+    Commander commander(options, service, *it, &input, &socket);
+
+    commander.Start();
+
+    service.run();
+  }
 
   return 0;
 }
