5bbcb5caed1595ca2904c8431fc4ce4c790c7df9
Josh Pieper <jjp@pobox.com>
2015-11-26 14:36:12 -0500

    gimbal: Add some newlib stub functions, and disable the independent watchdog.  There's no need to have it spuriously triggering while I'm trying to bring up the build at all.

--- hw/gimbal/firmware/Src/main.c
+++ hw/gimbal/firmware/Src/main.c
@@ -69,6 +69,27 @@ caddr_t _sbrk(int incr) {
   return 0;
 }
 
+int _close(int file) { return -1; }
+
+struct stat;
+int _fstat(int file, struct stat *st) { return -1; }
+
+int _isatty(int file) { return 1; }
+
+int _lseek(int file, int ptr, int dir) { return 0; }
+
+int _open(const char *name, int flags, int mode) { return -1; }
+
+int _read(int file, char* ptr, int len) { return -1; }
+
+int _write(int file, char *ptr, int len) { return -1; }
+
+void _exit(int foo) { while (1) {} }
+
+int _kill(int foo) { return -1; }
+
+int _getpid() { return -1; }
+
 /* USER CODE END 0 */
 
 int main(void)
@@ -93,7 +114,8 @@ int main(void)
   MX_I2C1_Init();
   MX_I2C2_Init();
   MX_I2C3_Init();
-  MX_IWDG_Init();
+  /* TODO jpieper: Re-enable this when we want watchdog support. */
+  /* MX_IWDG_Init(); */
   MX_SPI1_Init();
   MX_TIM1_Init();
   MX_TIM2_Init();
