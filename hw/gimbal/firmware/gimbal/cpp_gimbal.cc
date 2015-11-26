df5cd3d449c58c8f77072f92a441c792be1c2092
Josh Pieper <jjp@pobox.com>
2015-11-26 14:49:31 -0500

    gimbal: Make the lights blink that exist on the discovery board, and do so at a rate that is observable to humans.

--- hw/gimbal/firmware/gimbal/cpp_gimbal.cc
+++ hw/gimbal/firmware/gimbal/cpp_gimbal.cc
@@ -13,13 +13,35 @@
 // limitations under the License.
 
 #include "gpio.h"
+#include "iwdg.h"
 
 extern "C" {
 void cpp_gimbal_main() {
   // For now, lets try to flash an LED with no sleeps or anything.
+  uint32_t old_tick = 0;
+  int cycle = 0;
   while (1) {
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+    HAL_IWDG_Refresh(&hiwdg);
+    uint32_t new_tick = HAL_GetTick();
+    if (new_tick != old_tick && (new_tick % 250) == 0) {
+      cycle = (cycle + 1) % 8;
+      if (cycle & 1) {
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+      } else {
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+      }
+      if (cycle & 2) {
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+      } else {
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+      }
+      if (cycle & 4) {
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+      } else {
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+      }
+    }
+    old_tick = new_tick;
   }
 }
 }
