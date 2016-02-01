c29c14c1b91aa01635e11cca6509f490519ab956
theamk <code@theamk.com>
2016-02-01 00:07:25 -0500

    video receiver code. not hooked up to video decoder yet.

--- mech/mcast_video_link.cc
+++ mech/mcast_video_link.cc
@@ -15,6 +15,8 @@
 #include "mcast_video_link.h"
 
 #include <mutex>
+#include <bitset>
+#include <unordered_map>
 #include <thread>
 
 #include <gst/gst.h>
@@ -84,6 +86,62 @@ struct DataPacketHeader {
 };
 
 BOOST_STATIC_ASSERT(sizeof(DataPacketHeader) == DataPacketHeader::kSize);
+
+
+class DataPacket : boost::noncopyable {
+ public:
+  DataPacket() {};
+
+  // Create new packet
+  DataPacket(const DataPacketHeader& hdr,
+             const char* video_data, int video_len)
+      : data_(hdr.video_start + video_len, 0) {
+    BOOST_ASSERT(video_len >= 0);
+    BOOST_ASSERT(hdr.video_start >= sizeof(hdr));
+    ::memcpy(&data_[0], &hdr, sizeof(hdr));
+    ::memcpy(&data_[hdr.video_start], video_data, video_len);
+    CheckValid();
+  }
+
+  // Parse from bytes
+  DataPacket(const std::string& data)
+      : data_(data) {
+    CheckValid();
+  }
+
+  // Accessors
+  bool valid() { return valid_; };
+
+  // Mutable header
+  DataPacketHeader& header() {
+    BOOST_ASSERT(valid_);
+    return *reinterpret_cast<DataPacketHeader*>(&data_.front());
+  }
+  const std::string& data() {
+    BOOST_ASSERT(valid_);
+    return data_;
+  }
+
+ private:
+  void CheckValid() {
+    if (data_.size() <= DataPacketHeader::kSize) {
+      valid_ = false;
+      return;
+    }
+    const DataPacketHeader& dph =
+        *reinterpret_cast<DataPacketHeader*>(&data_.front());
+    valid_ = (dph.magic == DataPacketHeader::kMagic &&
+              dph.video_start >= sizeof(DataPacketHeader) &&
+              dph.video_start < data_.size() &&
+              dph.total_packets > 0 &&
+              dph.packet_index < dph.total_packets &&
+              dph.repeat_count > 0 &&
+              dph.repeat_index < dph.repeat_count);
+  }
+
+  bool valid_ = false;
+  std::string data_;
+};
 
 struct DataPacketLog {
   boost::posix_time::ptime timestamp;
@@ -181,12 +239,10 @@ class McastVideoLinkTransmitter::Impl : boost::noncopyable {
         hdr.packet_index = index;
         int this_data_size = std::min(data_size, len - offset);
         BOOST_ASSERT(this_data_size > 0);
+
         to_tx_.emplace_back(
-            reinterpret_cast<const char*>(&hdr), sizeof(hdr));
-        to_tx_.back().resize(hdr.video_start + this_data_size);
-        ::memcpy(&to_tx_.back().front() + hdr.video_start,
-                 reinterpret_cast<char*>(data) + offset,
-                 this_data_size);
+            hdr, reinterpret_cast<char*>(data) + offset, this_data_size);
+        BOOST_ASSERT(to_tx_.back().valid());
         offset += this_data_size;
       }
       BOOST_ASSERT(offset == len);
@@ -210,24 +266,21 @@ class McastVideoLinkTransmitter::Impl : boost::noncopyable {
  private:
   void TxNextPacket(boost::system::error_code ec) {
     base::FailIf(ec);
+    BOOST_ASSERT(std::this_thread::get_id() == main_id_);
     std::lock_guard<std::mutex> guard(queue_mutex_);
 
     BOOST_ASSERT(tx_running_);
     BOOST_ASSERT(!to_tx_.empty());
 
-    DataPacketHeader* header = reinterpret_cast<DataPacketHeader*>(
-        &to_tx_.front().front());;
     boost::posix_time::ptime now = base::Now(service_);
-    header->tx_time_us = base::ConvertPtimeToMicroseconds(now);
+    to_tx_.front().header().tx_time_us = base::ConvertPtimeToMicroseconds(now);
 
     DataPacketLog dpl;
     // Transmit packet
     dpl.timestamp = now;
-    dpl.header = *header;
-    link_->Send(to_tx_.front());
+    dpl.header = to_tx_.front().header();
+    link_->Send(to_tx_.front().data());
     to_tx_.pop_front();
-
-    //log_.debug("TX packet, %d in queue", to_tx_.size());
 
     // Schedule next packet if needed.
     dpl.queue_len = to_tx_.size();
@@ -256,14 +309,14 @@ class McastVideoLinkTransmitter::Impl : boost::noncopyable {
   const Parameters& parameters_;
   std::thread::id main_id_;
   bool started_ = false;
-  base::LogRef log_ = base::GetLogInstance("mcast_video_link");
+  base::LogRef log_ = base::GetLogInstance("mcast_video_tx");
   std::unique_ptr<base::UdpDataLink> link_;
   std::mutex queue_mutex_;
 
   // Queue-related variables. Protected by queue_mutex_.
   uint32_t total_frames_ = 0;
   uint32_t last_iframe_num_ = 0;
-  std::deque<std::string> to_tx_;
+  std::deque<DataPacket> to_tx_;
   // All packets must be sent by this time.
   boost::posix_time::ptime tx_deadline_;
   base::DeadlineTimer tx_timer_;
@@ -324,6 +377,110 @@ std::weak_ptr<CameraFrameConsumer
 //
 //                          RECEIVER
 //
+namespace {
+
+struct ReceivedFrameInfo {
+  // time when the last packet for the frame is received
+  // (this may be after the frame has been passed to a decoder)
+  boost::posix_time::ptime timestamp;
+  // time from the first packet to the time it was passed to a decoder
+  // (or to a last packet if the frame was never complete)
+  double rx_latency_s = 0;
+
+  // number for this frame and for the latest i-frame
+  uint32_t frame_num = 0;
+  uint32_t iframe_num = 0;
+
+  // data packets sent (counting all duplicates only once)
+  int total_packets = 0;
+  // data packets lost -- if this is non-zero, frame is not passed to decoder.
+  int lost_packets = 0;
+
+  // repeat count for this frame
+  int repeat_count = 0;
+  // average number of copies received
+  double avg_rx_copies = 0;
+
+  template <typename Archive>
+  void Serialize(Archive* a) {
+    a->Visit(MJ_NVP(timestamp));
+    a->Visit(MJ_NVP(rx_latency_s));
+
+    a->Visit(MJ_NVP(frame_num));
+    a->Visit(MJ_NVP(iframe_num));
+
+    a->Visit(MJ_NVP(total_packets));
+    a->Visit(MJ_NVP(lost_packets));
+
+    a->Visit(MJ_NVP(repeat_count));
+    a->Visit(MJ_NVP(avg_rx_copies));
+  }
+};
+typedef boost::signals2::signal<void (const ReceivedFrameInfo*)
+                                > ReceivedFrameInfoSignal;
+
+class RxFrameBuffer : boost::noncopyable {
+ public:
+  RxFrameBuffer(const DataPacketHeader& header) {
+    info_.frame_num = header.frame_num;
+    // TODO theamk recover iframe_num
+    info_.iframe_num = 0;
+    info_.total_packets = header.total_packets;
+    info_.repeat_count = header.repeat_count;
+
+    BOOST_ASSERT(info_.total_packets <= static_cast<int>(missing_.size()));
+    packets_.resize(info_.total_packets);
+    for (int i=0; i<info_.total_packets; i++) {
+      missing_.set(i);
+    }
+    info_.lost_packets = missing_.count();
+  }
+
+  // Is the frame complete (do we have all packets?)
+  bool complete() const { return info_.lost_packets == 0; };
+
+  // ReceivedFrameInfo structure, suitable for logging.
+  const ReceivedFrameInfo* info() {
+    return &info_;
+  }
+
+  // Handle the packet. it will be moved 'in' if it is interesting.
+  void HandlePacket(const boost::posix_time::ptime now,
+                    std::unique_ptr<DataPacket>* packet) {
+    const DataPacketHeader& header = (*packet)->header();
+    BOOST_ASSERT(info_.frame_num == header.frame_num);
+    BOOST_ASSERT(info_.total_packets == header.total_packets);
+    BOOST_ASSERT(info_.repeat_count == header.repeat_count);
+
+    info_.timestamp = now;
+    if (first_rx_time_.is_not_a_date_time()) {
+      first_rx_time_ = now;
+    }
+    if (!complete()) {
+      info_.rx_latency_s = base::ConvertDurationToSeconds(
+          now - first_rx_time_);
+    }
+
+    if (missing_[header.packet_index]) {
+      // This is a new packet. keep it.
+      BOOST_ASSERT(missing_.test(header.packet_index));
+      missing_.reset(header.packet_index);
+      info_.lost_packets = missing_.count();
+      packets_[header.packet_index].swap(*packet);
+    }
+    rx_count_ += 1;
+    info_.avg_rx_copies = rx_count_ * 1.0 / info_.total_packets;
+  }
+ private:
+  ReceivedFrameInfo info_;
+  boost::posix_time::ptime first_rx_time_ = boost::posix_time::not_a_date_time;
+  std::vector<std::unique_ptr<DataPacket>> packets_;
+  std::bitset<64> missing_;
+  int rx_count_ = 0;
+};
+
+};
+
 
 class McastVideoLinkReceiver::Impl : boost::noncopyable {
  public:
@@ -337,25 +494,104 @@ class McastVideoLinkReceiver::Impl : boost::noncopyable {
   }
 
   void AsyncStart(base::ErrorHandler handler) {
+    link_.reset(new base::UdpDataLink(
+                    service_, log_, parameters_.link));
+    link_->data_signal()->connect(
+        std::bind(&Impl::HandlePacket, this, std::placeholders::_1,
+                  std::placeholders::_2));
     started_ = true;
   }
 
   DataPacketSignal* packet_signal() { return &packet_signal_; }
+  ReceivedFrameInfoSignal* frame_info_signal() { return &frame_info_signal_; }
 
  private:
+  void ExpireOldFrames() {
+    // Remove oldest frames
+    while (pending_deque_.size() > 3) {
+      int id = pending_deque_.front();
+      auto it = pending_.find(id);
+      BOOST_ASSERT(it != pending_.end());
+      frame_info_signal_(it->second->info());
+
+      pending_deque_.pop_front();
+      pending_.erase(it);
+    }
+  }
+
+  void HandlePacket(const std::string& data,
+                    const base::UdpDataLink::PeerInfo& peer) {
+    std::unique_ptr<DataPacket> packet(new DataPacket(data));
+    if (!packet->valid()) {
+      log_.warn("got invalid packet (%d bytes) from %d",
+                data.size(), peer.id);
+      return;
+    }
+
+    const DataPacketHeader* header = &packet->header();
+    if (0) {
+      log_.debug("got %d bytes from %d (frame %d, pkt %d/%d, rep %d/%d)",
+                 data.size(), peer.id, header->frame_num,
+                 header->packet_index, header->total_packets,
+                 header->repeat_index, header->repeat_count);
+    }
+    boost::posix_time::ptime now = base::Now(service_);
+
+    // Log the packet before RxFrameBuffer graphs it.
+    DataPacketLog dpl;
+    dpl.timestamp = now;
+    dpl.header = *header;
+    dpl.queue_len = 0;
+    if (!last_rx_time_.is_not_a_date_time()) {
+      dpl.interval_s = base::ConvertDurationToSeconds(now - last_rx_time_);
+    }
+    last_rx_time_ = now;
+    packet_signal_(&dpl);
+
+    // Find RxFrameBuffer or construct new if not found.
+    auto p_it = pending_.find(header->frame_num);
+    RxFrameBuffer* buff = nullptr;
+    if (p_it == pending_.end()) {
+      // Create a new frame buffer
+      buff = new RxFrameBuffer(*header);
+      pending_[header->frame_num] = std::unique_ptr<RxFrameBuffer>(buff);
+      pending_deque_.push_back(header->frame_num);
+    } else {
+      buff = p_it->second.get();
+    }
+    bool was_complete = buff->complete();
+
+    buff->HandlePacket(now, &packet);
+    // 'packet' is invalid after this point.
+
+    if (buff->complete() && !was_complete) {
+      // packet received. pass to decoder
+      log_.debug("packet %d complete", header->frame_num);
+    }
+    ExpireOldFrames();
+  }
+
   DataPacketSignal packet_signal_;
+  ReceivedFrameInfoSignal frame_info_signal_;
   McastVideoLinkReceiver* const parent_;
   boost::asio::io_service& service_;
   const Parameters& parameters_;
   std::thread::id main_id_;
   bool started_ = false;
-  base::LogRef log_ = base::GetLogInstance("mcast_video_link");
+  base::LogRef log_ = base::GetLogInstance("mcast_video_rx");
+  std::unique_ptr<base::UdpDataLink> link_;
+
+  boost::posix_time::ptime last_rx_time_ = boost::posix_time::not_a_date_time;
+  std::unordered_map<uint32_t, std::unique_ptr<RxFrameBuffer>> pending_;
+  std::deque<uint32_t> pending_deque_;
 };
 
 
 McastVideoLinkReceiver::McastVideoLinkReceiver(base::Context& context)
     : impl_(new Impl(this, context.service)) {
   context.telemetry_registry->Register("mvl_data_rx", impl_->packet_signal());
+  context.telemetry_registry->Register(
+      "mvl_frame_rx", impl_->frame_info_signal());
 }
 
 McastVideoLinkReceiver::~McastVideoLinkReceiver() {}
