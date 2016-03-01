66e4fe768dbe33a8485f62d947cd1f86f7534192
theamk <code@theamk.com>
2016-03-01 16:50:56 -0500

    create tools/start-control.sh (from mw_command_invoke.txt)

---
+++ tools/start-control.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+jsname=$(readlink -e /dev/input/event16)
+if [[ "$jsname" == "" ]]; then
+    echo Fatal: no joystick found
+    exit 1
+fi
+mkdir -p logs
+now=$(date +%F-%H%M%S)
+set -x
+./mech/build-x86_64/mw_command \
+    --log logs/mwc-$now.klg -L --display.write_video logs/mwv-$now.mp4 \
+    --opt.verbose 1 --joystick $jsname \
+    --target 10.89.0.10 \
+    --cmd.body_y_mm 10 --opt.max_translate_x_mm_s 200 --opt.max_translate_y_mm_s 200 \
+    "$@"
