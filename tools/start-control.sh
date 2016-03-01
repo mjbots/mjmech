e459bec27f5af3236089507a8e35375ad6536d14
Josh Pieper <jjp@pobox.com>
2016-03-01 17:09:30 -0500

    Update joystick device name.

--- tools/start-control.sh
+++ tools/start-control.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-jsname=$(readlink -e /dev/input/event16)
+jsname=$(readlink -e \
+	/dev/input/by-id/usb-Logitech_Logitech_Dual_Action-event-joystick)
 if [[ "$jsname" == "" ]]; then
     echo Fatal: no joystick found
     exit 1
@@ -8,7 +9,7 @@ mkdir -p logs
 now=$(date +%F-%H%M%S)
 set -x
 ./mech/build-x86_64/mw_command \
-    --log logs/mwc-$now.klg -L --display.write_video logs/mwv-$now.mp4 \
+    --log logs/mwc-$now.klg -L --display.write_video logs/mwv-$now.mkv  \
     --opt.verbose 1 --joystick $jsname \
     --target 10.89.0.10 \
     --cmd.body_y_mm 10 --opt.max_translate_x_mm_s 200 --opt.max_translate_y_mm_s 200 \
