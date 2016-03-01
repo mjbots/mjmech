cd20d98a645e003e0ea29ace4f0d5a890125971b
theamk <code@theamk.com>
2016-03-01 00:00:22 -0500

    tools/start-robot: make automatic source rebuild optional

--- tools/start-robot.sh
+++ tools/start-robot.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-# A script to run the remote (controller) part
-# runs script directly on odroid, uses ssh on pc
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    cat <<EOF
+A script to run the remote (controller) part of the mjmech system.
+Runs script directly on odroid, uses ssh on pc.
+
+Any arguments are passed to a remote side, except for the leading:
+  -q   (quick) -- do not automatically recompile software
+EOF
+    exit 1
+fi
+
 set -e
 
 mach=$(uname -m)
@@ -9,18 +18,26 @@ cd $(dirname $(dirname $(readlink -f $0)))
 
 if [[ "$mach" == "armv7l" && "$USER" == "odroid" ]]; then
     echo Compiling source
+    if [[ "$1" == "-q" ]]; then
+        shift
+    else
+        (set -x;
+            scons -j2 mech/build-armv7l/mech_warfare)
+    fi
     set -x
-    scons -j2 mech/build-armv7l/mech_warfare
     mech/build-armv7l/mech_warfare -t camera_driver.stats \
-         -c configs/mw.ini  "$@"
+         -c configs/mw.ini "$@"
 elif [[ "$mach" == "x86_64" && "$USER" != "odroid" ]]; then
     # make sure we can compile our copy
-    set -x
-    scons -j8 mech/build-x86_64/video_sender_app
+    if [[ "$1" != "-q" ]]; then
+        (set -x;
+            scons -j8 mech/build-x86_64/mech_warfare)
+    fi
     # Use wlan IP unless specified otherwise
     # to use wires, prefix with: REMOTE=odroid-mjmech
     : ${REMOTE:=10.89.0.10}
     # upload and run
+    set -x
     exec tools/odroid/push-tree.sh $REMOTE tools/start-robot.sh "$@"
 else
     echo cannot determine what kind of machine it is
