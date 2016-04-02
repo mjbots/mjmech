1060a5a43013533cba61aa444e4f22bb633ded67
theamk <code@theamk.com>
2016-04-01 23:43:05 -0400

    Support REMOTE env in start-control.sh

--- tools/start-control.sh
+++ tools/start-control.sh
@@ -8,10 +8,14 @@ fi
 mkdir -p logs
 now=$(date +%F-%H%M%S)
 set -x
+
+# Use REMOTE env var to set remote IP
+: ${REMOTE:=10.89.0.10}
+
 ./mech/build-x86_64/mw_command \
     --log logs/mwc-$now.klg -L --display.write_video logs/mwv-$now.mp4  \
     --opt.verbose 1 --joystick $jsname \
     -t video_display.stats --display.stats_interval_s 30 \
-    --target 10.89.0.10 \
+    --target $REMOTE \
     --opt.max_translate_x_mm_s 200 --opt.max_translate_y_mm_s 200 \
     "$@"
