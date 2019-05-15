e2cb47fc68d00cabcf70bf22318b3ffc51387c8f
Josh Pieper <jjp@pobox.com>
2019-05-14 22:51:32 -0400

    Update start-control

--- utils/start-control.sh
+++ utils/start-control.sh
@@ -10,12 +10,16 @@ now=$(date +%F-%H%M%S)
 set -x
 
 # Use REMOTE env var to set remote IP
-: ${REMOTE:=10.89.0.10}
+: ${REMOTE:=192.168.16.47}
 
-./mech/build-x86_64/mw_command \
-    --log logs/mwc-$now.klg -L --display.write_video logs/mwv-$now.mp4  \
+./bazel-out/k8-opt/bin/mech/mw_command \
+    --log logs/mwc-$now.klg \
     --opt.verbose 1 --joystick $jsname \
     -t video_display.stats --display.stats_interval_s 30 \
     --target $REMOTE \
-    --opt.max_translate_x_mm_s 200 --opt.max_translate_y_mm_s 200 \
+    --video_link.dest $REMOTE \
+    --remote_debug.port 9999 \
+    --video_link.source 192.168.16.42:2180 \
+    --opt.max_translate_x_mm_s 100 --opt.max_translate_y_mm_s 100 \
+    --opt.max_rotate_deg_s=10 \
     "$@"
