48a6542b0ab99ad7fdab6159eba284ef4c78330e
Josh Pieper <jjp@pobox.com>
2019-08-01 07:20:04 -0400

    Try various different tweaks to the travis config

--- travis-ci.sh
+++ travis-ci.sh
@@ -5,5 +5,7 @@ set -ev
 sudo apt-get update
 ./install-packages --yes
 
-./tools/bazel test //...
-./tools/bazel build -c opt --config=pi //mech:deploy.tar
+./tools/bazel test //base/...
+
+#./tools/bazel test //...
+#./tools/bazel build -c opt --config=pi //mech:deploy.tar
