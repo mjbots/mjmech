e2dbdff344b6714ed2522219adac1ac79dd19a6f
Josh Pieper <jjp@pobox.com>
2019-09-23 09:50:13 -0400

    Update update_pid to have a more useful base set of commands

--- utils/update_pid.sh
+++ utils/update_pid.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for device in 1 2 4 5 7 8 10 11; do
+for device in 3 6 9 12; do
     echo $device
-    (echo "conf set servo.pid_position.kp 1300";
+    (echo "conf set servo.pid_position.kp 2200";
      sleep 0.05;
      echo "conf set servo.pid_position.kd 9";
      sleep 0.2;
